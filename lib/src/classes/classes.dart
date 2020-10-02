7cf35295b2393395abca462ab4629c5c838ee91d
Igor Bragaia <igor.bragaia@gmail.com>
2020-10-01 23:00:13 -0300

    Add missing server mockup file

---
+++ lib/src/classes/classes.dart
@@ -0,0 +1,1 @@
+export './server_mockup.dart';
---
+++ lib/src/classes/server_mockup.dart
@@ -0,0 +1,51 @@
+import 'dart:math';
+
+import 'package:mobync/src/models/models.dart';
+
+class ServerMockup {
+  ServerMockup._privateConstructor();
+  static final ServerMockup instance = ServerMockup._privateConstructor();
+
+  /// Mockup for the SyncOperation lists from each user
+  Map<String, List<SyncOperation>> syncOperationListByOwner = {};
+
+  /// Mockup for the LogicalClock from each user
+  Map<String, int> logicalClockByOwner = {};
+
+  void _updateLogicalClockForOwner(String owner, int clientLogicalClock) {
+    if (!logicalClockByOwner.containsKey(owner)) logicalClockByOwner[owner] = 0;
+
+    logicalClockByOwner[owner] =
+        max(logicalClockByOwner[owner], clientLogicalClock) + 1;
+  }
+
+  Future<ServerSyncResponse> sync({
+    String owner,
+    int clientLogicalClock,
+    List<SyncOperation> userSyncOperationList,
+  }) {
+    if (!syncOperationListByOwner.containsKey(owner)) {
+      syncOperationListByOwner[owner] = [];
+    }
+    if (!logicalClockByOwner.containsKey(owner)) {
+      logicalClockByOwner[owner] = 0;
+    }
+
+    if (userSyncOperationList.length > 0) {
+      _updateLogicalClockForOwner(owner, clientLogicalClock);
+
+      /// Silly merge, just sort by logical clock and timestamp.
+      syncOperationListByOwner[owner].addAll(userSyncOperationList);
+      syncOperationListByOwner[owner].sort();
+    }
+
+    var syncOperationResponseList = syncOperationListByOwner[owner]
+        .where((el) => el.logicalClock > clientLogicalClock)
+        .toList();
+    var response = ServerSyncResponse(
+      logicalClock: logicalClockByOwner[owner],
+      syncOperationList: syncOperationResponseList,
+    );
+    return Future.value(response);
+  }
+}
